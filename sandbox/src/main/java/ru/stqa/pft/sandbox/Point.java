94449ac8c65db00a4be2223579683266fb44a790
Antimka <antimkin@gmail.com>
2020-05-30 14:28:32 +0300

    Homework - Point - added comments

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,17 +6,19 @@ public class Point {
   public double x;
   public double y;
 
-  //eto konstruktor
+  //eto konstruktor dlja koordinat tochki
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
 
   }
 
+  //eto cho-to iz interneta, chtoby zadatj vyvod koordinat tochki na ekran
   public String toString() {
     return "(" + this.x + "," + this.y + ")";
   }
 
+  //eto to, chto sdelali drugie - screen v skype
   public static double distance(Point p1, Point p2) {
     double x = p2.x - p1.x;
     double y = p2.y - p1.y;
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -2,15 +2,19 @@ package ru.stqa.pft.sandbox;
 
 public class PointDistance {
 
-//  public static void main(String[] args) {
-//
-//    Point p1 = new Point(6, 5);
-//    Point p2 = new Point(9, 9);
-//
-//    System.out.println("Rasstojanie mezhdu tochkami " + "("+ p1.x + "," + p1.y + ") i ("+ p2.x + "," + p2.y + ") = " + distance(p1, p2));
+  public static void main(String[] args) {
 
-public static void main(String[] args){
-  Point point= new Point(3,2);
-  System.out.println(point.toString());
-}
+    //popytka rasschitatj rastojanie
+    Point p1 = new Point(6, 5);
+    Point p2 = new Point(9, 9);
+
+    System.out.println("Rasstojanie mezhdu tochkami " + "(" + p1.x + "," + p1.y + ") i (" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+
+    //vyvod koordinat tochki na ekran - chto-to iz interneta
+    public static void main (String[]args){
+
+      Point point = new Point(3, 2);
+      System.out.println(point.toString());
+    }
+  }
 }
