230c3954958c13887573411c7f27c37f4562aaee
Antimka <antimkin@gmail.com>
2020-06-28 16:00:18 +0300

    Contact Deletion test added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -39,4 +39,16 @@ public class ContactHelper extends HelperBase {
   public void goToConcactPage() {
     click(By.linkText("add new"));
   }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
+
+  public void deleteSelectedContact() {
+    click(By.xpath("//input[@value='Delete']"));
+  }
+
+  public void submitContactDeletion() {
+    wd.switchTo().alert().accept();
+  }
 }
