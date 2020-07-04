a8b409616a606815059cc89644e72361b077e8cb
Antimka <antimkin@gmail.com>
2020-07-04 11:54:47 +0300

    added Edge browser

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,10 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,9 +17,27 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new ChromeDriver();
+    //tut boljshe lokaljnaja peremennaja ne nuzhna -
+    // vmesto budet ispoljzovan attribut objekta, kotorij byl ispoljzovan pri konstruirovanii etogo objekta
+
+    System.setProperty("webdriver.edge.driver","C:\\Users\\ugisr\\Desktop\\Anna_Java_cource\\installs_and_exe\\msedgedriver.exe"); //put actual location
+    //String browser = BrowserType.EDGE;
+    if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    } else if (browser == BrowserType.EDGE) {
+      wd = new EdgeDriver();
+  }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
